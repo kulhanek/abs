0aa8ec2b227aba926e6bac628cf875ed0ee252e4
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-03-30 15:06:15 +0200

    submit

--- src/lib/abs/resources/ResourceList.cpp
+++ src/lib/abs/resources/ResourceList.cpp
@@ -138,7 +138,7 @@ void CResourceList::AddResource(const CSmallString& name,const CSmallString& val
     CSimpleIteratorC<CPluginObject> I(PluginDatabase.GetObjectList());
     CPluginObject* p_pobj;
     while( (p_pobj = I.Current()) ){
-        std::cout << "res >: " << endl;
+        std::cout << "res >: " << p_pobj->GetCategoryUUID().GetFullStringForm() << endl;
         if( p_pobj->GetCategoryUUID() == RESOURCES_CAT ){
             std::cout << "res >: " << p_pobj->GetObjectUUID().GetDescription() <<  std::endl;
             CSmallString res_name = p_pobj->GetObjectUUID().GetDescription();
