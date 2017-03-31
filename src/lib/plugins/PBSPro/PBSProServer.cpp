0d2b0c0345dce70be5af152f056ac68da1b32e9d
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-03-31 20:06:40 +0200

    submit

--- src/lib/plugins/PBSPro/PBSProServer.cpp
+++ src/lib/plugins/PBSPro/PBSProServer.cpp
@@ -697,10 +697,10 @@ bool CPBSProServer::SubmitJob(CJob& job)
     }
     if( mailoptions == NULL ) mailoptions = "n";
 
-//    set_attribute(p_prev,ATTR_m,NULL,mailoptions);
-//    set_attribute(p_prev,ATTR_v,NULL,variables);
+    set_attribute(p_prev,ATTR_m,NULL,mailoptions);
+    set_attribute(p_prev,ATTR_v,NULL,variables);
 
-  //  CreateJobAttributes(p_prev,&(job.ResourceList));
+    CreateJobAttributes(p_prev,&(job.ResourceList));
 
     if( depjid != NULL ){
         depjid = "afterany:" + depjid;
