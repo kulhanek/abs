2c9fd2a38871cda49b10918e18f742b8bbcbc170
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-04-02 20:04:04 +0200

    submit

--- src/lib/plugins/PBSPro/PBSProQueue.cpp
+++ src/lib/plugins/PBSPro/PBSProQueue.cpp
@@ -58,12 +58,9 @@ bool CPBSProQueue::Init(const CSmallString& short_srv_name,struct batch_status* 
     get_attribute(p_queue->attribs,ATTR_total,NULL,TotalJobs);
     get_attribute(p_queue->attribs,ATTR_rescmax,"walltime",MaxWallTime);
 
-    if( get_attribute(p_queue->attribs,ATTR_rescdflt,"walltime",DefaultWallTime) == false ){
+    if( get_attribute(p_queue->attribs,ATTR_rescdflt,"walltime",DefaultWallTime,true) == false ){
         DefaultWallTime = MaxWallTime;
-        cout << "here " << endl;
     }
-
-        cout << "defa walltime = " << DefaultWallTime.GetSTime() << endl;
 
     get_attribute(p_queue->attribs,ATTR_routedest,NULL,RouteDestinations);
     get_attribute(p_queue->attribs,ATTR_comment,NULL,Comment);
