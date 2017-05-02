0a4a2f4b68bce679b6b3d9e76456ac37e0dcc3aa
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-05-02 19:27:38 +0200

    resources

--- src/lib/abs/resources/ResourceList.cpp
+++ src/lib/abs/resources/ResourceList.cpp
@@ -277,8 +277,6 @@ void CResourceList::RemoveResource(const CSmallString& name)
         }
     }
 
-    cout << "r-res:" << name << " fname: " << fname << " s: " << srv << endl;
-
     std::list<CResourceValuePtr>::iterator     it = begin();
     std::list<CResourceValuePtr>::iterator     ie = end();
 
